900065fdaf49a42362db9f3c80d983f2e6f6b35e
MayaBgt <maya.bogatskaya@gmail.com>
2025-11-07 11:47:02 -0500

    refactor: centralized regex patterns and updated test classes

--- src/test/java/com/mb/api/tests/utils/TestDataLoader.java
+++ src/test/java/com/mb/api/tests/utils/TestDataLoader.java
@@ -2,7 +2,6 @@ package com.mb.api.tests.utils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 import java.io.InputStream;
 
